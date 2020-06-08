b690c0338596c4af69efa30e3cdbf3362ce31eee
Kevin Garcia <kg.gar91@gmail.com>
2020-06-08 20:38:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,10 @@
 first_number = 10
 second_number = 5
 
-sum = ${first_number} + ${second_number}   
+sum = ${first_number} + ${second_number}
 
 difference = ${first_number} - ${second_number}
 
 product = ${first_number} * ${second_number}
 
 quotient = ${first_number} - ${second_number}
-
